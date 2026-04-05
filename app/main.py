6ff6401fccce6ebdca48744c4dbfb58fb2418eba
nugusu20 <nugusu14@gmail.com>
2026-04-05 19:56:16 +0300

    ci: add github actions pipeline

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.api.routes import router
+
+app = FastAPI()
+
+app.include_router(router)
+
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI CI/CD project running"}
